5d204debcb742c49c62bdb6a4c5f23faeceff3c6
Austin Burgard <awburgard@gmail.com>
2019-05-11 18:07:39 -0500

    finished

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,9 +1,10 @@
 const galleryItems = [
-    { id:0 , path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id:0 , path: 'images/goat_small.jpg', description: 'A goat -- is it screaming?', likes: 0 },
     { id:1 , path: 'images/owl.jpg', description: 'Photo of an awesome owl.', likes: 0 },
     { id:2 , path: 'images/boulder.jpg', description: 'Photo of an awesome boulder.', likes: 0 },
     { id:3 , path: 'images/cow.jpg', description: 'This is a red cow', likes: 0 },
     { id:4 , path: 'images/leopard.jpg', description: 'Scary leopard', likes: 0 },
+    { id:5 , path: 'images/blues.jpg', description: 'LETS GO BLUES!', likes: 0 },
 ];
 
 module.exports = galleryItems;
